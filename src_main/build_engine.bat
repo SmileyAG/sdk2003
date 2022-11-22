465481c36dc0a0c8b535cad10a7b9ff0c0677bdd
SmileyAG <58108407+SmileyAG@users.noreply.github.com>
2022-11-22 07:36:13 +0400

    Edit build_engine.bat

--- src_main/build_engine.bat
+++ src_main/build_engine.bat
@@ -7,30 +7,12 @@
 @call start_build %1 %2 %3
 
 
-@rem
-@rem Choose debug or release build
-@rem
-if "%build_type%"=="debug" goto build_debug
-goto build_release
-
-
-:build_release
+goto build_releasedebug
 
 %MSDEV% engine/engine.dsp %CONFIG%"engine - Win32 Release" %build_target%
 if errorlevel 1 set BUILD_ERROR=1
 
-%MSDEV% dedicated/dedicated.dsp %CONFIG%"Dedicated - Win32 Release" %build_target%
-if errorlevel 1 set BUILD_ERROR=1
-
-goto done
-
-
-:build_debug
-
 %MSDEV% engine/engine.dsp %CONFIG%"engine - Win32 Debug" %build_target%
-if errorlevel 1 set BUILD_ERROR=1
-
-%MSDEV% dedicated/dedicated.dsp %CONFIG%"Dedicated - Win32 Debug" %build_target%
 if errorlevel 1 set BUILD_ERROR=1
 
 goto done
