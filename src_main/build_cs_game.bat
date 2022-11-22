aeb8315b3c3f93d46e7b32b5114db7b03e5e1876
SmileyAG <58108407+SmileyAG@users.noreply.github.com>
2022-11-22 07:42:12 +0400

    Edit build_cs_game.bat

--- src_main/build_cs_game.bat
+++ src_main/build_cs_game.bat
@@ -6,26 +6,16 @@
 @if "%build_level%"=="" setlocal
 @call start_build %1 %2 %3
 
-
-@rem
-@rem Choose debug or release build
-@rem
-if "%build_type%"=="debug" goto build_debug
-goto build_release
+goto build_releasedebug
 
 
-:build_release
+:build_releasedebug
  
 %MSDEV% dlls/hl.dsp %CONFIG%"hl - Win32 Release CounterStrike" %build_target%
 if errorlevel 1 set BUILD_ERROR=1
 
 %MSDEV% cl_dll/client.dsp %CONFIG%"client - Win32 Release CounterStrike" %build_target%
 if errorlevel 1 set BUILD_ERROR=1
-
-goto done
-
-
-:build_debug
 
 %MSDEV% dlls/hl.dsp %CONFIG%"hl - Win32 Debug CounterStrike" %build_target%
 if errorlevel 1 set BUILD_ERROR=1
