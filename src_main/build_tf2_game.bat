11d1738e57b8c2cd7f6e9f36d572b8517fb448ac
SmileyAG <58108407+SmileyAG@users.noreply.github.com>
2022-11-22 07:39:23 +0400

    Edit build_tf2_game.bat

--- src_main/build_tf2_game.bat
+++ src_main/build_tf2_game.bat
@@ -7,25 +7,15 @@
 @call start_build %1 %2 %3
 
 
-@rem
-@rem Choose debug or release build
-@rem
-if "%build_type%"=="debug" goto build_debug
-goto build_release
+goto build_releasedebug
 
-
-:build_release
+:build_releasedebug
 
 %MSDEV% dlls/hl.dsp %CONFIG%"hl - Win32 Release tf2" %build_target%
 if errorlevel 1 set BUILD_ERROR=1
 
 %MSDEV% cl_dll/client.dsp %CONFIG%"client - Win32 Release TF2" %build_target%
 if errorlevel 1 set BUILD_ERROR=1
-
-goto done
-
-
-:build_debug
 
 %MSDEV% dlls/hl.dsp %CONFIG%"hl - Win32 Debug tf2" %build_target%
 if errorlevel 1 set BUILD_ERROR=1
